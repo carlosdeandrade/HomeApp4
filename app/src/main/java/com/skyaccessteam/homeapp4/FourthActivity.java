1a38d74aa1ff17a349c47483735fc5568572b9fc
carlosdeandrade <candrade.7j@gmail.com>
2017-05-31 16:41:15 +0100

    31/05 16:40
    agregando pagina 4 con insights the wifi

--- app/src/main/java/com/skyaccessteam/homeapp4/FourthActivity.java
+++ app/src/main/java/com/skyaccessteam/homeapp4/FourthActivity.java
@@ -2,12 +2,30 @@ package com.skyaccessteam.homeapp4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 public class FourthActivity extends AppCompatActivity {
+
+    TextView hola;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fourth);
+
+        hola = (TextView)findViewById(R.id.textView55);
+
+        final String baseUrl4 = "http://192.168.0.1/sky_wlvis_site_survey.asp";
+
+
+        webView = (WebView)findViewById(R.id.wv_dummy3);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(baseUrl4);
+
     }
 }
