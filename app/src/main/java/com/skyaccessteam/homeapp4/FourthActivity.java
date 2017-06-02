a4ab06633a8b970028b73ff255af9928df73c747
carlosdeandrade <candrade.7j@gmail.com>
2017-06-02 10:39:57 +0100

    Getting SSIDs from http://192.168.0.1/sky_wlvis_site_survey.asp
    partially done

--- app/src/main/java/com/skyaccessteam/homeapp4/FourthActivity.java
+++ app/src/main/java/com/skyaccessteam/homeapp4/FourthActivity.java
@@ -2,14 +2,20 @@ package com.skyaccessteam.homeapp4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.ValueCallback;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class FourthActivity extends AppCompatActivity {
 
     TextView hola;
     WebView webView;
+    Button button55;
+    private static final String TAG= "homeAppDebug";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +23,11 @@ public class FourthActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fourth);
 
         hola = (TextView)findViewById(R.id.textView55);
+        button55 = (Button)findViewById(R.id.button55);
 
         final String baseUrl4 = "http://192.168.0.1/sky_wlvis_site_survey.asp";
+        final String baseUrl2 = "http://admin:sky@192.168.0.1/sky_router_status.html";
+        final String baseUrl5 = "http://192.168.0.1/wlvis/main.js";
 
 
         webView = (WebView)findViewById(R.id.wv_dummy3);
@@ -27,5 +36,68 @@ public class FourthActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(baseUrl4);
 
+
+
+        button55.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                getTextPag1();
+                //getTextPage2();
+            }
+        });
+
+
+
     }
+
+
+    private void getTextPage2() {
+
+
+        String elementId = "header";
+       // webView.
+        webView.evaluateJavascript(
+                "(function() { return document.getElementById('" + elementId + "').textContent; })();",
+                new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String html2) {
+
+                        String WIFIPass;
+
+                        // --- WIFI Pass ---
+                       // WIFIPass = html2.substring(1, html2.length()-1);
+                        hola.setText(html2);
+                        // --- end Manufacturer ---
+
+
+
+                    }
+                });
+
+    }
+
+
+    private void getTextPag1() {
+        String elementId = "rssigraphplaceholder";
+        webView.evaluateJavascript(
+                "(function() { return document.getElementById('" + elementId + "').textContent; })();",
+                new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String html) {
+                        String Manufacturer;
+
+                        // --- Manufacturer ---
+                        //Manufacturer = html.substring(html.indexOf("Manufacturer") + 17 , html.indexOf("Manufacturer") + 30);
+                        hola.setText(html);
+                        Log.d(TAG, html);
+                        // --- end Manufacturer ---
+
+                    }
+                });
+
+    }
+
+
+
 }
