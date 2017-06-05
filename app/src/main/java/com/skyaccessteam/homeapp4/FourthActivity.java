9049c2fc89aeda302711d38062c8d6e1cc47bda5
carlosdeandrade <candrade.7j@gmail.com>
2017-06-05 18:28:01 +0100

    adding a generic bar graph (partial)

--- app/src/main/java/com/skyaccessteam/homeapp4/FourthActivity.java
+++ app/src/main/java/com/skyaccessteam/homeapp4/FourthActivity.java
@@ -12,12 +12,30 @@ import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.DataSet;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class FourthActivity extends AppCompatActivity {
 
     TextView hola;
     WebView webView;
     TextView nOfNetworks;
     Button button55;
+    LineChart lineChart;
+    BarChart barChart;
+
+    HorizontalBarChart HorChar;
+
     private static final String TAG= "homeAppDebug";
     private static final int TAG2= 0;
 
@@ -30,6 +48,8 @@ public class FourthActivity extends AppCompatActivity {
         hola.setMovementMethod(new ScrollingMovementMethod());
         button55 = (Button)findViewById(R.id.button55);
         nOfNetworks = (TextView)findViewById(R.id.nOfNetworks);
+        lineChart = (LineChart)findViewById(R.id.graph2);
+        barChart = (BarChart)findViewById(R.id.graph);
 
         final String baseUrl4 = "http://192.168.0.1/sky_wlvis_site_survey.asp";
         final String baseUrl2 = "http://admin:sky@192.168.0.1/sky_router_status.html";
@@ -42,6 +62,69 @@ public class FourthActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(baseUrl4);
 
+
+        //---- Starts the Bars Graph
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(104f, 0));
+
+        entries.add(new BarEntry(80f, 1));
+        entries.add(new BarEntry(60f, 2));
+        entries.add(new BarEntry(100f, 3));
+        entries.add(new BarEntry(50f, 4));
+        entries.add(new BarEntry(30f, 5));
+        entries.add(new BarEntry(80f, 6));
+        entries.add(new BarEntry(60f, 7));
+        entries.add(new BarEntry(100f, 8));
+        entries.add(new BarEntry(50f, 9));
+
+        entries.add(new BarEntry(100f, 11));
+        entries.add(new BarEntry(50f, 12));
+        entries.add(new BarEntry(30f, 13));
+
+        BarDataSet dataset = new BarDataSet(entries, "Channels");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("1");
+        labels.add("2");
+        labels.add("3");
+        labels.add("4");
+        labels.add("5");
+        labels.add("6");
+        labels.add("7");
+        labels.add("8");
+        labels.add("9");
+        labels.add("10");
+        labels.add("11");
+        labels.add("12");
+        labels.add("13");
+
+//        BarChart chart = new BarChart(this);
+//        setContentView(chart);
+
+        BarData data = new BarData(labels, dataset);
+        barChart.setData(data);
+
+        barChart.setDescription("2.4GHz Frequency Spectrum");
+
+        barChart.setTouchEnabled(true);
+        barChart.setDragEnabled(true);
+        barChart.setScaleEnabled(true);
+        dataset.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        //---- Ends the Bars Graph
+
+
+//        double x=0;
+//        int numDataPoints = 1000;
+//
+//        for (int i=0; i<numDataPoints; i++) {
+//            float sinFunction = Float.parseFloat(String.valueOf(Math.sin(x)));
+//            float cosFunction = Float.parseFloat(String.valueOf(Math.cos(x)));
+//            x=x+0.1;
+//            yAXESsin.add(new Entry(sinFunction,i));
+//            yAXEScos.add(new Entry(cosFunction,i));
+//
+//        }
 
 
         button55.setOnClickListener(new View.OnClickListener(){
